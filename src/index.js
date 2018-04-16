d7ceaa5d2b16cbabcb94a04c317d59de8da28f60
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-16 13:31:14 +0200

    Fixed import actions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions';
+import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
