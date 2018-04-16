efa047823cfd26805b53d9eb186a112f1c94cb33
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-16 14:11:44 +0200

    Separation of routes to a separate file routes.js, delete no use imports

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import { getCountries } from './actions/actions-countries';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
+
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes} />
             <DevTools />
         </div>
     </Provider>,
